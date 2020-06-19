809e77e4f7780c68bf4bc5e9484e7c13c0dff348
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-06-19 13:17:48 +0200

    Modify the ChangeCubeColorSystem to look more like the OOP version

--- Assets/Scripts/Systems/ChangeCubeColorSystem.cs
+++ Assets/Scripts/Systems/ChangeCubeColorSystem.cs
@@ -1,38 +1,31 @@
 ﻿using UnityEngine;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Rendering;
 
 [UpdateBefore(typeof(ExplodeCubeSystem))]
 [UpdateAfter(typeof(SpawnCubeSystem))]
 public class ChangeCubeColorSystem : SystemBase
 {
-    private Unity.Mathematics.Random random;
- 
-    protected override void OnCreate() 
-    {
-        random = new Unity.Mathematics.Random(56);
-    }
     protected override void OnUpdate()
     {
-        // The ChangeColors method is run on every entity that holds both the RenderMesh and the Cube component
+        ChangeColors();
+    }
+   
+    // Randomize rgb values and set it to the entities material
+    private void ChangeColors()
+    {
+        // This code will run on each entity with a RenderMesh component and a Cube component (i.e on every cube in the scene). 
         Entities
             .WithoutBurst()
             .ForEach((RenderMesh renderMesh, ref Cube cubeColor) => 
         {
-            ChangeColors(renderMesh, cubeColor);
+            cubeColor.r = Random.Range(0f, 1f);
+            cubeColor.g = Random.Range(0f, 1f);
+            cubeColor.b = Random.Range(0f, 1f);
+
+            renderMesh.material.color = new Color(cubeColor.r, cubeColor.g, cubeColor.b);
 
         }).Run();
-
-    }
-    // Randomize rgb values and set it to the entities material
-    private void ChangeColors(RenderMesh renderMesh, Cube cubeColor) // TODO: Change and set the Entities foreach in the method
-    {
-        cubeColor.r = random.NextFloat(0f, 1f);
-        cubeColor.g = random.NextFloat(0f, 1f);
-        cubeColor.b = random.NextFloat(0f, 1f);
-
-        renderMesh.material.color = new Color(cubeColor.r, cubeColor.g, cubeColor.b);
     }
     
 }
