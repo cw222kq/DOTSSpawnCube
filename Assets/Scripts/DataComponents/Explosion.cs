e125b590d270587f4952f48418f802874a3492fd
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-06-19 13:00:37 +0200

    Delete unused system

--- Assets/Scripts/DataComponents/Explosion.cs
+++ Assets/Scripts/DataComponents/Explosion.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using Unity.Entities;
- //Added class ExplosionAuthoring, if this class is removed uncomment this to genereate the authoring component automatic
+
 [GenerateAuthoringComponent]
 public struct Explosion : IComponentData
 {
     public Entity spherePrefab;
-    [HideInInspector] public float spherePrefabYvalue;
     [HideInInspector] public float delay;
     [HideInInspector] public float countdown;
     [HideInInspector] public bool hasExplosionEntity;
+    [HideInInspector] public float spherePrefabYvalue;
     [HideInInspector] public bool hasExplode;
 }
