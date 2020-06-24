3f530c674e73fa62d3fe21bf1607326cac983fb8
Charlotte Westerberg <cw222kq@student.lnu.se>
2020-06-24 16:36:08 +0200

    Add a new system and component that will end the simulation after 45 seconds

---
+++ Assets/Scripts/DataComponents/Simulation.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent] 
+public struct Simulation : IComponentData
+{
+   public float endSimulationTimer;
+}
---
+++ Assets/Scripts/Systems/EndSimulationSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class EndSimulationSystem : SystemBase
+{
+    private Simulation simulation;
+
+    protected override void OnCreate() 
+    {
+        // End simulation after 45 seconds
+        simulation.endSimulationTimer = 45f;
+    }
+    protected override void OnUpdate()
+    {
+        simulation.endSimulationTimer -= Time.DeltaTime;
+
+        // 45 seconds after start end the simulation
+        if (simulation.endSimulationTimer <= 0f)
+        {
+            Debug.Log("QUIT!!!!!!");
+            Application.Quit();
+        }
+    }
+    
+}
